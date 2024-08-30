cdb82cb1c638dff3833662860e3a85c2aa166922
Veynamer <113140575+Veynamer@users.noreply.github.com>
2024-08-30 15:44:16 +0300

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -6,12 +6,9 @@ source build/envsetup.sh
 export USE_CCACHE=1
 breakfast $DEVICE
 make -j$(nproc) mkbootimg
-make -j$(nproc) hybris-boot
 make -j$(nproc) halium-boot
-make -j$(nproc) recoveryimage
 make -j$(nproc) systemimage 
 
 echo "md5sum halium-boot.img and system.img"
 md5sum $ANDROID_ROOT/out/target/product/rolex/halium-boot.img
-md5sum $ANDROID_ROOT/out/target/product/rolex/recovery.img
 md5sum $ANDROID_ROOT/out/target/product/rolex/system.img
